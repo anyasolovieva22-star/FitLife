a2490ddab4361d8424fe7ac7ce6c7e0f73c4ab38
Anna Solovieva <anyasolovieva22@mail.ru>
2026-07-29 22:55:52 +0300

    2.0

--- fit_life.py
+++ fit_life.py
@@ -1,46 +1,88 @@
 # Проект FitLife - MVP версия 1.0
 
 
-# 1. Знакомство
+class User:
+    """Знакомство с новым пользователем
+    Атрибуты:
+    LINE (constant): обозначает необходимое количество "-", для читаемости
+    текста.
+    user_name (str): вводит информацию об имени пользователя
+    user_age (int): вводит информацию о возрасте пользователя
+    """
+
+
+LINE = "-" * 45
+
+
 print('ДОБРО ПОЖАЛОВАТЬ в FitLife!')
-print("-" * 45)
+print(LINE)
 print('FitLife помогает заботиться о вашем здоровье!')
-# Узнаем имя пользователя
 user_name = input('Давай познакомимся, напиши как тебя зовут: ')
-# Узнаем возраст пользователя
-user_age = int(input('Укажи свой возраст: '))
+user_name = user_name.title()
+user_age = int(input('Укажи количество полных лет: '))
 
 
-# 2. Сбор данных
-print(f"Отлично, {user_name}!")
+class Info:
+    """Собираем данные о весе и росте для дальнейших расчетов
+    Атрибуты:
+    user_weight (float): вес в килограммах
+    user_height (float): рост в метрах
+    Если пользователь ошибется и использует ',', метод .replace() исправляет
+    проблему. Обязательно перед (float), так как этот метод используется в
+    (str)
+    """
+
+
+print(f"Отлично, {user_name.title()}!")
 print('Мы уже на половине пути к твоей цели!')
 print("\n")
-# Узнаем ВЕС пользователя в киллограммах
-user_weight = float((input('Для нужных расчетов, нам нужно знать твой вес, в'
-                           ' (кг): ')))
-# Узнаем РОСТ пользователя в метрах
-user_height = float((input('Теперь укажи свой рост в метрах, используя точку'
-                           ' (например: 1.65): ')))
+user_weight = (input('Для нужных расчетов, укажи свой вес, в (кг): '))
+user_weight = user_weight.replace(',', '.')
+user_weight = float(user_weight)
+user_height = (input('Укажи свой рост в метрах, используя точку (например:'
+                     ' 1.65): '))
+user_height = user_height.replace(',', '.')
+user_height = float(user_height)
 
 
-# 3. Логика расчетов
-# ---Расчет ИМТ---
+class Calculation:
+    """Производим расчеты Индекса Массы Тела и нормы воды
+    Формула ИМТ:
+        1)bmi (float): индекс массы тела = user_weight / (user_height ** 2).
+        2)bmi_result (float): округляем bmi до 1 цифры после точки с помощью
+        round (bmi, 1)
+
+    Формула нормы воды:
+        Для расчета потребуется:
+        WATER_PER_KG (constant): суточная норма воды в миллилитрах
+        WATER_L (constant): 1 литр воды в миллилитрах
+
+    1)water_needed (float): норма воды = (user_weight * WATER_PER_KG) / WATER_L
+    2)water_nedeed_result (float): округляем water_needed до 1 цифры после
+    точки с помощью round (water_needed, 1)
+    """
+
+
 WATER_PER_KG = 30
 WATER_L = 1000
-# Формула ИМТ: вес разделить на (рост в квадрате)
+
+
 bmi = user_weight / (user_height ** 2)
 bmi_result = round(bmi, 1)
-# ---Расчет нормы воды---
 water_needed = (user_weight * WATER_PER_KG) / WATER_L
 water_needed_result = round(water_needed, 1)
 
-# 4. Результат
-print("\n" + "-" * 45)
+
+class Result:
+    """Выводит пользователю готовый результат"""
+
+
+print("\n" + LINE)
 print(f"{user_name}!, вот твой результат!")
 print(f"Пользователь: {user_name}, {user_age} г. ")
 print(f"Индекс Массы Тела: {bmi_result}")
 print(f"Рекомендуемая норма воды: {water_needed_result} л. в день")
-print("-" * 45)
+print(LINE)
 
 
 print("Расчет окончен. Будьте здоровы!")
